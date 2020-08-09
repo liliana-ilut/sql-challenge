68020ba50ca9073f3349db50eab454df53055c14
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-08-09 14:48:10 -0500

    final changes

--- EmployeeSQL/Working_Folder/SQL queries/tables_schema.sql
+++ EmployeeSQL/Working_Folder/SQL queries/tables_schema.sql
@@ -1,47 +1,50 @@
+--this query will create the schema, import the CSV files and check if the files were imported correctly
+--create the ERD Diagram, the export it over here
+
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL
 );
-drop table employees 
+
 CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
+    "emp_no" int   NOT NULL,
     "emp_title_id" varchar   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "gender" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
 );
-drop table titles
+
 CREATE TABLE "titles" (
     "title_id" varchar   NOT NULL,
-    "title" VARCHAR   NOT NULL
+    "title" varchar   NOT NULL
 );
-SELECT * FROM titles;
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -61,3 +64,13 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
 REFERENCES "employees" ("emp_title_id");
 
+--check if all tables open correctly using "select * "function
+
+select * from departments;
+select * from dept_emp;
+select * from dept_manager;
+select * from employees;
+select * from salaries;
+select * from titles;
+
+-- every tables was imported successfully, we open another query (queries.sql) to run the 8 questions
